2a89f1222a04b6e57a03fa3bbe8e679267354df1
rosen <you@example.com>
2021-12-04 23:10:55 +0700

    useEffect -deps

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -4,31 +4,47 @@
 //2.useEffect(callback, [])
 //-Chi goi calback 1 lan sau khi component mounted
 //3.useEffect(callback, [deps])
+//-callback se duoc goi lai moi khi dependencies thay doi
 import {useEffect, useState} from 'react'
-
+const tabs = ['posts', 'comments', 'albums'];
 
 function Content(){
     const [title, setTitle] = useState('');
     const [posts, setPosts] = useState([]);
+    const [type, setType] = useState('posts');
   
     useEffect(()=>{
-        document.title = title;
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(res=>res.json())
         .then(posts => {
             setPosts(posts)
         })
-    }, []);
+    }, [type]);
 
     return (
         <div>
+            {tabs.map(tab=>
+                <button style={
+                    type===tab?{
+                        backgroundColor:'#333',
+                        color:'#fff',
+                    }:{}
+                }
+
+                key={tab}
+                onClick={()=>setType(tab)}  
+                >{tab}</button>
+                           
+                )           
+            }
             <input
             value={title}
             onChange={e => setTitle(e.target.value)}            
             ></input>
             <ul>
                 {posts.map(post =>
-                     <li key={post.id}>{post.title}</li>
+                     <li key={post.id}>{post.title || post.name}</li>
                 )}
             </ul>
         </div>
