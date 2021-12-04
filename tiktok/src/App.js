0d035a97375893e6ca2077adcfd0dbc6ead883ab
rosen <you@example.com>
2021-12-04 17:00:07 +0700

    radio-input

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,15 +1,40 @@
 import {useState} from 'react'
+const courses = [
+  {id: 1, name: 'HTML CSS'},
+  {id: 2, name: 'JS'},
+  {id: 3, name: 'reactJS'}, 
+
+]
+
 
 function App() {
-
+const [checked, setChecked] = useState();
+const handleSubmit = () => {
+  console.log({id: checked})
+}
 return (
-  <div style={{padding: 32}}>
- 
+  <div style={{padding:32}}>
+  {
+    courses.map((course) =>(
+      <div key={course.id}>
+      <input
+          type="radio"
+          checked={checked===course.id}
+          onChange={()=>setChecked(course.id)}  
+      
+      />
+      {course.name}      
+      </div>
+     
+    ))
+    
+  }
+  <button onClick={()=>handleSubmit()}>Submit</button>
+  
+  
   </div>
-
-
-
 )
+
  }
 
  
