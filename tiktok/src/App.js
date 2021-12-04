32bf10d8c550a2c56865ce7bdcb4a67667bae3bd
rosen <you@example.com>
2021-12-04 16:20:00 +0700

    delete random

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,15 +1,10 @@
 import {useState} from 'react'
+
 function App() {
-  const [counter, setCounter] = useState(1)
-  const handleIncrease = () => {
-    setCounter(counter+1)
-  }
-  return (
-    <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-    </div>
-  );
-}
+
+ }
+
+ 
+
 
 export default App;
