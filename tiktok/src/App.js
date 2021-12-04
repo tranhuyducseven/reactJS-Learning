5d6d3e5e568bb8f40a35e158bb01b1cab6a684df
rosen <you@example.com>
2021-12-04 16:27:46 +0700

    random Gift

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,7 +1,23 @@
 import {useState} from 'react'
-
+const gifts = [
+  'CPU i9',
+  'rgb ram',
+  'logitech mouse'
+]
 function App() {
+  const [gift, setGift] = useState();
+  const randomGift = () => {
+    setGift(gifts[Math.floor(Math.random() *3)]);
+  }
+return (
+  <div style={{padding: 32}}>
+  <h1>{gift ||'Have no gift'}</h1>
+  <button onClick={randomGift}>Get gift</button>
+  </div>
 
+
+
+)
  }
 
  
