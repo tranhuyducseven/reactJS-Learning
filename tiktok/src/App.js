785c7a5b765eb9808ed3a4d4b91b912a19a2140d
rosen <you@example.com>
2021-12-06 16:29:06 +0700

    useMemo

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,43 +1,53 @@
-import {useState} from 'react'
-const courses = [
-  {id: 1, name: 'HTML CSS'},
-  {id: 2, name: 'JS'},
-  {id: 3, name: 'reactJS'}, 
-
-]
-
+import { useState, useRef, useMemo } from 'react';
 
 function App() {
-const [checked, setChecked] = useState();
-const handleSubmit = () => {
-  console.log({id: checked})
-}
-return (
-  <div style={{padding:32}}>
-  {
-    courses.map((course) =>(
-      <div key={course.id}>
-      <input
-          type="radio"
-          checked={checked===course.id}
-          onChange={()=>setChecked(course.id)}  
-      
-      />
-      {course.name}      
-      </div>
-     
-    ))
-    
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
+  const nameRef = useRef();
+  const handleSubmit = () => {
+    setProducts(prev => [...prev, {
+      name,
+      price: Number(price)
+    }]);
+    setName('');
+    setPrice('');
+    nameRef.current.focus();
   }
-  <button onClick={()=>handleSubmit()}>Submit</button>
-  
-  
-  </div>
-)
+  let total = useMemo(() =>
+  products.reduce((result, product) => result + product.price, 0)
+  ,[products])
+  return (
+    <div style={{ padding: 32 }}>
+      <input
+        ref={nameRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter name"
+      />
+      <br />
+      <input
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+        placeholder="Enter price"
+      />
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product, index) =>
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>)}
+      </ul>
+    </div>
 
- }
+  )
 
- 
+}
+
+
 
 
 export default App;
